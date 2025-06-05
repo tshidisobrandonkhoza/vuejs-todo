646601023ad43cc0bda5f64b33845b74a68af3ba
tshidisobrandonkhoza <8brandonkhoza@gmail.com>
2025-06-05 18:39:36 +0200

    Options setup and basic directives

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<!-- options must export default with scripst -->
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'To Do',
+      status: true,
+      tasks: ['Task A', 'Task B', 'Task C']
+    };
+  },
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <p v-if="status">User Active</p>
+  <p v-if="!status">User Not Active</p>
+  <button>Show Task</button>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="task">{{ task }} - {{ index }}</li>
+  </ul>
+</template>
